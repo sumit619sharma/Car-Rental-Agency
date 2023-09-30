d97bafb1e46a213480ada2e7457103dd5c34effb
sumit619sharma <imsumitsharma619@gmail.com>
2023-09-30 18:12:18 +0530

    change

--- car-rental-backend/app.js
+++ car-rental-backend/app.js
@@ -15,22 +15,7 @@ const authRouter = require('./routes/auth');
 const carsRouter = require('./routes/cars');
 const bookingRouter = require('./routes/booking');
 
-app.get('/favicon.ico', (req, res , next) => {
-  // Customize the response for /favicon.ico here
-  // For example, you can send a custom favicon file or a 204 No Content response
-  // to indicate that there is no favicon available.
-  // Replace the following with your desired behavior:
 
-  // Send a custom favicon file
-  // res.sendFile(__dirname + '/path/to/your/favicon.ico');
-
-  // OR
-
-  // Send a 204 No Content response
-  // res.status(204).end();
-res.send('hello there');
-  //next();
-}); 
 
 
 app.use( (req,res)=> {
